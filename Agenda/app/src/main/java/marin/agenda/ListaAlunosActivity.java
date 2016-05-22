99e8337b14e3ca1d463e2ebe53d996a9678503fb
Anderson Marin <dam.marinramos@gmail.com>
2016-05-22 17:13:10 -0300

    ListViews

--- Agenda/app/src/main/java/marin/agenda/ListaAlunosActivity.java
+++ Agenda/app/src/main/java/marin/agenda/ListaAlunosActivity.java
@@ -2,6 +2,8 @@ package marin.agenda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ListaAlunosActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class ListaAlunosActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_alunos);
+
+        String[] alunos  = {"Bianca","Spike","Charlie","Charlote"};
+        ListView listaAlunos = (ListView) findViewById(R.id.lista_alunos);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos);
+        listaAlunos.setAdapter(adapter);
     }
 }
