62669294f5b7f0ad56c6bc80291d318952f2d7a8
Anderson Marin <dam.marinramos@gmail.com>
2016-05-30 22:10:51 -0300

    Working with buttons and menus

--- Agenda/app/src/main/java/marin/agenda/FormularioActivity.java
+++ Agenda/app/src/main/java/marin/agenda/FormularioActivity.java
@@ -1,10 +1,16 @@
 package marin.agenda;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import java.util.List;
 
 public class FormularioActivity extends AppCompatActivity {
 
@@ -12,13 +18,24 @@ public class FormularioActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
+    }
 
-        Button botaoSalvar = (Button) findViewById(R.id.formulario_salvar);
-        botaoSalvar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(FormularioActivity.this, "Botão clicado", Toast.LENGTH_SHORT).show();
-            }
-        });
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_formulario, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    //executa quando o botão de salvar do menu é clicado
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_formulario_ok:
+                Toast.makeText(FormularioActivity.this, "Salvo", Toast.LENGTH_SHORT).show();
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
